20ca24ce0954c1c64bb45343725b8070ebc8224d
Shir Bar Lev <shirblc@gmail.com>
2024-06-16 18:08:56 +0100

    Added sample styling to the AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,7 @@ import { RouterOutlet } from "@angular/router";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
+  styleUrl: "./app.component.less",
   standalone: true,
   imports: [RouterOutlet, CommonModule],
 })
