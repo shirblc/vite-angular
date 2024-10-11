e2659631d4c3512c28c4d0e5a1b62d37df1c9f24
Shir Bar Lev <shirblc@gmail.com>
2024-10-11 15:37:27 +0300

    Missed a plugin reference.

--- plugins/wtr.js
+++ plugins/wtr.js
@@ -38,7 +38,7 @@ export const AngularTestsPlugin = () => {
           include: ["src/**/*.ts"],
           exclude: ["node_modules/**", "src/**/*.spec.ts"],
         }),
-        addCompiler(),
+        addCompilerPlugin(),
       ]);
       await ngBuilder.setupCompilerHost();
       ngBuilder.setupAngularProgram();
